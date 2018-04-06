9a5fb8d0fde35b8a428ec7b1763583f0a353f97e
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-04-06 11:02:13 +0800

    OK

--- 2018.4.5集训赛/E.Base Station Sites.cpp
+++ 2018.4.5集训赛/E.Base Station Sites.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
+#include <cstdio>
 #include <algorithm>
+#define N 100050
 using namespace std;
-const int N = 1e5 + 10;
-int S,L,a[N];
-int main()
-{
-    while(scanf("%d%d",&L,&S)&&L+S){
-        for(int i=0;i<L;i++) scanf("%d",&a[i]);
-        sort(a,a+L);
-        int l = 0, r = 1e6,mid;
-        while(l<=r){
-            mid = (l+r)>>1;
-            int cnt = 1, now = 0;
-            for(int i=1;i<L;i++){
-                if(a[i]-a[now]>=mid){
-                    now = i; cnt++;
-                }
-            }
-            if(cnt>=S) l = mid + 1;
-            else r = mid - 1;
-        }
-        printf("%d\n",l-1);
-    }
-    return 0;
+int a[N];
+int main()	{
+	int n,m;
+	while (scanf("%d %d",&n,&m)&&n&&m)	{
+		for (int i=0;i<n;i++)	scanf("%d",&a[i]);
+		sort(a,a+n);
+		int left=0,right=a[n-1]-a[0];
+		while (left<=right)	{
+			int mid=(left+right)/2;
+			int now=0,cnt=0;
+			for (int i=0;i<n;i++)	{
+				if (a[i]-a[now]>=mid)	{
+					cnt++;
+					now=i;
+				}
+			}
+			if (cnt>=m-1)	left=mid+1;
+			else if (cnt<m-1)	right=mid-1;
+		}
+		printf("%d\n",left-1);
+	}
+	return 0;
 }
-
--- 2018.4.5集训赛/F.Nearby Bicycles.cpp
+++ 2018.4.5集训赛/F.Nearby Bicycles.cpp
@@ -3,20 +3,41 @@
 #include <cmath>
 #define N 1005		
 using namespace std;
+/*
 typedef struct poin{
 	double x;
 	double y;
 }point;
+*/
 double rad[N];
 double db(double x)	{
 	return x*x;
 }
+struct point{
+    double x,y;
+    void write(){
+        char c;
+        while ((c=getchar())==' '||c=='\n');
+        scanf("%lf,%lf)",&x,&y);
+        getchar();
+    }
+};
 int main()	{
 	int n,m;
 	while (scanf("%d %d",&n,&m)==2&&n&&m)	{
 		int cnt[N];
 		point bic[N],peo[N];
 		for (int i=0;i<m;i++)	cnt[i]=0;
+		getchar();
+        for (int i = 0; i < n; i++) {
+            bic[i].write();
+           // bic[i].show();
+        }
+        for (int i = 0; i < m; i++) {
+            peo[i].write();
+            //per[i].show();
+        }
+		/*
 		for (int i=0;i<n;i++)	{
 			getchar();
 			getchar();
@@ -29,16 +50,16 @@ int main()	{
 			scanf("%lf,%lf",&peo[i].x,&peo[i].y);
 			getchar();
 		}
+		*/
 		for (int i=0;i<m;i++)	scanf("%lf",&rad[i]);
 		for (int i=0;i<m;i++)	{
 			for (int j=0;j<n;j++)	{
-				double distance;
 				double t1=bic[j].x-peo[i].x;
 				double xx=db(t1);
 				double t2=bic[j].y-peo[i].y;
 				double yy=db(t2);
-				distance=sqrt(xx+yy);
-				if (distance<=rad[i])	cnt[i]++;
+				double distance=(xx+yy);
+				if (distance<=rad[i]*rad[i])	cnt[i]++;
 			}
 		}
 		for (int i=0;i<m;i++)	{
