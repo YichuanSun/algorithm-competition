e75c61bacf7f7b86cc1b1afca1e4552be0688bb2
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-04-08 09:21:53 +0800

    .

--- UPC ACM Platform/1494 recursion 2的幂次方.cpp
+++ UPC ACM Platform/1494 recursion 2的幂次方.cpp
@@ -4,18 +4,40 @@ using namespace std;
 void fun(int n);
 int main()	{
 	int n;
-	while (scanf("%d",&n))	{
+	while (scanf("%d",&n)!=EOF)	{
 		fun(n);
+		printf("\n");
 	}
 	return 0;
-} 
-void fun(int n)	{
-	int cnt=0,ntemp=n;
-	while (ntemp)	{
-		ntemp>>=1;
-		cnt++;
-		if (ntemp<=n>>1)	break;
-	}
-	fun(ntemp);
-	
 }
+
+void fun(int n)	{
+	if (n==0)	{
+		printf("2(0)");
+		return;
+	}
+	else if (n==1)	{
+		printf("2");
+		return;
+	}
+	int s=1,cnt=0;
+	while (s<=n)	{
+		s<<=1;
+		cnt++;
+	}s>>=1,cnt--;	//s是不大于它的最大2幂次数，cnts是几次方
+	cout <<"s=" <<s<<endl;
+	cout <<"cnt=" <<cnt<<endl;
+	if (s==n)	{
+		printf("2(");
+		fun(s);
+		printf(")");
+	}
+	else if (s<=n)	{
+		printf("2(");
+		fun(n-s);
+		printf(")");
+	}
+}
+
+
+
